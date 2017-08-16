33dbf53386e349c22fe2b0ca2132806c7877c786
Mika Pruikkonen <mika.pruikkonen@iki.fi>
2017-08-16 15:31:13 +0300

    limit whole genome duplications to one

--- operations/whole_genome.jl
+++ operations/whole_genome.jl
@@ -1,23 +1,40 @@
-function whole_genome_duplication(cell::Cell)
-    new_chromosomes= Array{Chromosome, 1}(0)
-    mcount = 0
-    for chromosome in cell.chromosomes
-        push!(new_chromosomes, chromosome)
-        push!(new_chromosomes, chromosome)
-        for driver in chromosome.drivers
-            mcount += driver.gene.suppressor == false ? 1 : -1
+function already_duplicated(cell::Cell)
+    for m in cell.mutation_list
+        if m.op == whole_genome_duplication
+            return true
         end
     end
-    return new_chromosomes, cell.k+mcount, 0
+    return false
 end
 
-function whole_genome_duplication(segments, data)
-    print_msg("genome duplicated")
-    cidx = 1
-    ccount = size(segments)[1]
-    while ccount > 0
-        splice!(segments, cidx+1:cidx, [copy(segments[cidx])])
-        cidx += 2
-        ccount -= 1
+function whole_genome_duplication(cell::Cell)
+    if !already_duplicated(cell)
+        new_chromosomes= Array{Chromosome, 1}(0)
+        mcount = 0
+        for chromosome in cell.chromosomes
+            push!(new_chromosomes, chromosome)
+            push!(new_chromosomes, chromosome)
+            for driver in chromosome.drivers
+                mcount += driver.gene.suppressor == false ? 1 : -1
+            end
+        end
+        return new_chromosomes, cell.k+mcount, true
+    else
+        return cell.chromosomes, cell.k, false
+    end
+end
+
+function whole_genome_duplication(segments, first_dup)
+    if first_dup
+        print_msg("genome duplicated")
+        cidx = 1
+        ccount = size(segments)[1]
+        while ccount > 0
+            splice!(segments, cidx+1:cidx, [copy(segments[cidx])])
+            cidx += 2
+            ccount -= 1
+        end
+    else
+        print_msg("genome reduplication omitted")
     end
 end
